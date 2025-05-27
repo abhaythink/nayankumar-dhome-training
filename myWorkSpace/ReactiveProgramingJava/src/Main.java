481e5f8d7552d2703d709d8192ab34049b221169
nayankumar-thinkitive <nayankumar.dhome@thinkitive.com>
2025-05-27 13:24:36 +0530

    Added Day1 code

---
+++ myWorkSpace/ReactiveProgramingJava/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.Flow;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Flow.Publisher publisher = new MyPublisher();
+        Flow.Subscriber subscriber = new MySubscriber();
+
+        publisher.subscribe(subscriber);
+
+
+        Flow.Processor processor1 = new MyProcessor();
+        Flow.Processor processor2 = new MyProcessor();
+
+        publisher.subscribe(processor1);
+        processor1.subscribe(processor2);   // Processor extends both the Interfaces Publisher and Subscriber it works like a bridge
+        processor2.subscribe(subscriber);
+
+        // publisher -->  processor1 --> processor2 --> subscriber
+        // publisher always upstream
+        // processor1 is in downstream in terms of publisher
+        // processor1 is in upstream in terms of processor2
+        // processor2 is in downstream in terms of processor1
+        // processor2 is in upstream in terms of subscriber
+        // subscriber always downstream
+    }
+}
---
+++ myWorkSpace/ReactiveProgrammingPracticeDay1/src/main/java/com/nayankumar/demos/ReactiveProgramming/Practice/ReactiveProgrammingPracticeDay1/PracticeDay1.java
@@ -0,0 +1,6 @@
+package com.nayankumar.demos.ReactiveProgramming.Practice.ReactiveProgrammingPracticeDay1;
+
+public class PracticeDay1 {
+
+
+}
